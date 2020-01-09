e20bdf193fbb0cebb57026ad25b378395a758a78
Jorge Garcés <jgarcesembarba@gmail.com>
2020-01-09 12:14:21 +0100

    feature: approvals mutation testing done

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,28 @@
 package com.gildedrose;
 
-import org.junit.jupiter.api.Test;
+import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
-class GildedRoseTest {
+public class GildedRoseTest {
 
     @Test
-    void foo() {
+    public void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
+        CombinationApprovals.verifyAllCombinations(this :: doUpdateQuality,
+                new String[] {"foo", "Aged Brie", "Sulfuras, Hand of Ragnaros", "Backstage passes to a TAFKAL80ETC concert"},
+                new Integer[] {0, 10, 11,6, 1},
+                new Integer[] {0,  5, 2, 51, 49, 48, 50});
     }
-
+    private String doUpdateQuality(String name, int sellIn, int quality) {
+        Item[] items = new Item[] { new Item(name, sellIn, quality) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        return app.items[0].toString();
+    }
 }
